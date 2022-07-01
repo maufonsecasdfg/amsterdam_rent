3ff8e33096f2a2b129ac0b64152503cca7f50157
Mauricio Fonseca Fernández <maufonseca@gmail.com>
2022-06-30 22:15:50 -0600

    added yelp scraper

---
+++ yelp_scraper.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import requests
+from tqdm.notebook import tqdm
+
+from KEYS import YELP_API_KEY
+
+class YelpScraper():
+    def __init__(self):
+        self.known_bus = pd.read_csv('./data/yelp_businesses.csv')
+        self.cache = pd.read_csv('./data/yelp_cache.csv')
+        self.new_bus = pd.DataFrame()
+        self.coords = pd.read_csv('./data/postcode_coordinates.csv')
+
+    def get_businesses(self):
+        self.headers = {
+            'Authorization': f'Bearer {YELP_API_KEY}'
+        }
+        base_url = 'https://api.yelp.com/v3/businesses/search'
+        categories = ['active','arts','auto','beautysvc','bicycles','education',
+        'eventservices','financialservices','food','health','homeservices',
+        'hotelstravel','localflavor','localservices','massmedia','nightlife',
+        'pets','professional','publicservicesgovt','religiousorgs','restaurants','shopping']
+        for postcode in tqdm(self.coords['postcode']):
+            lat = self.coords[self.coords['postcode']==postcode]['latitude'].values[0]
+            lng = self.coords[self.coords['postcode']==postcode]['longitude'].values[0]
+
+            for cat in categories:
+                if cat not in self.cache[self.cache['postcode']==postcode]['category'].values:
+                    tries = 0
+                    while tries < 3:
+                        query = f'{base_url}?categories={cat}&latitude={lat}&longitude={lng}&radius=2000&limit=50'
+                        result = requests.get(query,headers=self.headers,verify='./consolidate.pem')
+                        if str(result.status_code)[0] == '2':
+                            break
+                        tries += 1
+                    if str(result.status_code)[0] == '2':
+                        busss = result.json()['businesses']
+                        if len(busss) != 0:
+                            df = pd.DataFrame(busss)
+                            if 'price' in df.columns:
+                                df = df[['id','name','url','coordinates','review_count','rating','price']]
+                            else:
+                                df = df[['id','name','url','coordinates','review_count','rating']]
+                                df['price'] = np.nan
+                            df = df.join(df['coordinates'].apply(pd.Series))
+                            df = df.drop(columns='coordinates')
+                            df['category'] = cat
+                            self.new_bus = pd.concat([self.new_bus,df])
+                    cache = pd.DataFrame({'postcode':[postcode],'category':[cat]})
+                    self.cache = pd.concat([self.cache,cache])
+                    
+        self.new_bus = self.new_bus.drop_duplicates()
+
+    def save_new_bus(self):
+        if (list(self.known_bus.columns) == ['id', 'name', 'url', 'review_count', 'rating', 'price', 'latitude','longitude', 'category'] 
+            and list(self.new_bus.columns) == ['id', 'name', 'url', 'review_count', 'rating', 'price', 'latitude','longitude', 'category']
+            and list(self.cache.columns) == ['postcode', 'category']):
+            self.known_bus = pd.concat([self.known_bus,self.new_bus]).drop_duplicates().reset_index(drop=True)
+            self.known_bus.to_csv('./data/yelp_businesses.csv',index=False)
+            self.cache.to_csv('./data/yelp_cache.csv',index=False)
+            self.new_bus = pd.DataFrame()
+        else:
+            print('Either known_bus, new_bus or cache dataframe has incorrect schema.')
+
