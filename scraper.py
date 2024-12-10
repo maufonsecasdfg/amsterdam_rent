a724c582fecff341d0804fcabb1fa5ae13c5d466
maufonseca <maufonseca@gmail.com>
2024-12-10 10:23:59 +0100

    debugging

--- scraper.py
+++ scraper.py
@@ -6,7 +6,8 @@ import time
 import pandas as pd
 from datetime import date
 import re
-from google.cloud import secretmanager
+import chardet
+#from google.cloud import secretmanager
 
 
 with open('config/bigquery_config.json', 'r') as f:
@@ -37,15 +38,15 @@ class Scraper():
             price_type = ' '.join(price_text.strip().split(' ')[1:])
         return price, price_type    
     
-    def get_proxies_from_secret(self, secret_name):
-        client = secretmanager.SecretManagerServiceClient()
-        name = f"projects/{bigquery_config['project_id']}/secrets/{secret_name}/versions/latest"
-        response = client.access_secret_version(request={"name": name})
-        return json.loads(response.payload.data.decode("UTF-8"))
+    #def get_proxies_from_secret(self, secret_name):
+    #    client = secretmanager.SecretManagerServiceClient()
+    #    name = f"projects/{bigquery_config['project_id']}/secrets/{secret_name}/versions/latest"
+    #    response = client.access_secret_version(request={"name": name})
+    #    return json.loads(response.payload.data.decode("UTF-8"))
 
-    def get_random_proxy(self, secret_name):
-        proxies = self.get_proxies_from_secret(secret_name)
-        return random.choice(proxies)
+    #def get_random_proxy(self, secret_name):
+    #    proxies = self.get_proxies_from_secret(secret_name)
+    #    return random.choice(proxies)
 
     def scrape(self, city, site, post_type, max_pages=10000):
         properties = []
@@ -64,11 +65,12 @@ class Scraper():
                     tries = 0
                     err = None
                     while tries <= 3:
-                        proxy_list_secret_name = "proxy-list" 
-                        proxy_url = self.get_random_proxy(proxy_list_secret_name)
-                        proxies = {"http://": proxy_url, "https://": proxy_url}
+                        #proxy_list_secret_name = "proxy-list" 
+                        #proxy_url = self.get_random_proxy(proxy_list_secret_name)
+                        #proxies = {"http://": proxy_url, "https://": proxy_url}
                         try:
-                            response = httpx.get(base_url+f'page-{page}', headers=headers, follow_redirects=True, timeout=20.0, proxies=proxies)
+                            #response = httpx.get(base_url+f'page-{page}', headers=headers, follow_redirects=True, proxies=proxies)
+                            response = httpx.get(base_url+f'page-{page}', headers=headers, follow_redirects=True)
                         except httpx.TimeoutException as errt:
                             print('                Timeout Error, retrying...')
                             err = errt
@@ -86,16 +88,15 @@ class Scraper():
                     if tries == 3:
                         if err is None:
                             print('            Reached final page (tries route).')
-                            print(response.text)
-                            print(response.__dict__)
                             break
                         else:
                             print(f'            Tries exceeded with error: {err}')
                             page += 1
                             continue
-                        
-                    print(response.__dict__)
-
+                    
+                    detected_encoding = chardet.detect(response.content)
+                    print(detected_encoding)
+                    response.encoding = 'utf-8' 
                     soup = BeautifulSoup(response.text,'html.parser')
                     page_propts = soup.find_all('li', class_="search-list__item search-list__item--listing")
 
@@ -170,8 +171,6 @@ class Scraper():
                         page += 1
                     else:
                         print('            Reached final page (no properties route).')
-                        print(response.text)
-                        print(response.__dict__)
                         break
 
     def run(self, cities, sites, post_types, max_pages=10000):
